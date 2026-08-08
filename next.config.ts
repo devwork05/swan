1ba669fa98a668800ecc942ec897f0fc4d003003
Dev Work05 <cc.devwork01@gmail.com>
2026-08-08 08:35:35 +0100

    latest

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  trailingSlash: true,
+  // trailingSlash: true,
   reactStrictMode: false,
     images: {
       unoptimized: true,
